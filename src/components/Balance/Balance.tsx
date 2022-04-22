582a6e14bbfb2377f7120910ffa55dc9b8da117f
Deepak Raaj Vaidyalingam <deepak.vaidyalingam@accenture.com>
2022-04-22 14:42:39 +0800

    Implemented the logic to show the balance amount so user will understand how to spend further

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import strings from "../../constants/Strings";
+import { RootState } from "../../state/reducer/root-reducer";
 
 const Balance = () => {
+  const { expenseList, budget } = useSelector(
+    (state: RootState) => state.expenseList
+  );
+
+  const spentAmount = expenseList.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
+  const alertType = spentAmount > budget ? "alert-danger" : "alert-success";
   return (
-    <div className="alert alert-success">
+    <div className={`alert ${alertType}`}>
       <span>
-        {strings.balanceAmount}: {strings.currency} 1000
+        {strings.balanceAmount}: {strings.currency} {budget - spentAmount}
       </span>
     </div>
   );
