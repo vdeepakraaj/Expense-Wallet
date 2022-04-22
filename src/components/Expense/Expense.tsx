e63939aa23629130c7df585806ffa31fe96081df
Deepak Raaj Vaidyalingam <deepak.vaidyalingam@accenture.com>
2022-04-22 13:18:14 +0800

    Implemented the logic to show the expense amount spent so far by adding the expenses amount

--- src/components/Expense/Expense.tsx
+++ src/components/Expense/Expense.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import strings from "../../constants/Strings";
+import { RootState } from "../../state/reducer/root-reducer";
 
 const Expense = () => {
+  const { expenseList } = useSelector((state: RootState) => state.expenseList);
+
+  const spentAmount = expenseList.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="alert alert-warning">
       <span>
-        {strings.spentAmount}: {strings.currency} 5000
+        {strings.spentAmount}: {strings.currency} {spentAmount}
       </span>
     </div>
   );
